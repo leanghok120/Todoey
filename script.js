e99a89068e1a5226c5d429fd69dccf5519af667b
leanghok120 <leanghokoeng5@gmail.com>
2024-08-13 05:41:45 +0700

    Switch to daisyui

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const addBtn = document.getElementById("addBtn");
-const taskInput = document.getElementById("taskInput");
 const taskLists = document.getElementById("taskLists");
+const taskInput = document.getElementById("taskInput");
+
+loadTasks();
 
 function addTask() {
-  const taskName = taskInput.value.trim();
-  if (taskName) {
+  const task = taskInput.value.trim();
+  if (task) {
     taskInput.value = "";
-    createTaskElement(taskName);
+
+    createTaskElement(task);
+    saveTask();
   }
 }
 
-function createTaskElement(task) {
+function createTaskElement(taskName) {
   const taskEl = document.createElement("li");
-  taskEl.textContent = task;
+  taskEl.className =
+    "bg-primary w-full h-16 rounded-md flex justify-center items-center text-neutral font-bold text-xl shadow-md";
+  taskEl.textContent = taskName;
   taskLists.appendChild(taskEl);
+}
+
+function saveTask() {
+  const tasks = [];
+  taskLists.querySelectorAll("li").forEach((item) => {
+    tasks.push(item.textContent);
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+
+  tasks.forEach((task) => {
+    createTaskElement(task);
+  });
 }
