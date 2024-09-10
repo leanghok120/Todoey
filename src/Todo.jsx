905d2c6eb07a0652e0dfc134db03eeba5dc7a5fe
leanghok120 <leanghokoeng5@gmail.com>
2024-09-10 19:53:07 +0700

    Add animations to list

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 function Todo() {
   const [tasks, setTasks] = useState(
     JSON.parse(localStorage.getItem("tasks")) || [],
   );
   const [newTask, setNewTask] = useState("");
+
+  const [listRef] = useAutoAnimate();
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -39,7 +42,11 @@ function Todo() {
           Add
         </button>
       </div>
-      <ul className="w-full mt-8 flex flex-col gap-2" id="todo-container">
+      <ul
+        className="w-full mt-8 flex flex-col gap-2"
+        id="todo-container"
+        ref={listRef}
+      >
         {tasks.map((task, index) => (
           <li
             className="bg-primary w-full h-16 rounded-md flex justify-center items-center text-neutral font-bold text-xl shadow-md cursor-pointer"
