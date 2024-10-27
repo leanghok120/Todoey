84a3baaa90ed99ca066f55199b4061c2ba564b84
leanghok120 <leanghokoeng5@gmail.com>
2024-10-27 18:40:55 +0700

    Setup basic crud operations

--- server/index.js
+++ server/index.js
@@ -1,9 +1,48 @@
 import express from "express";
+import mysql from "mysql";
+import "dotenv/config";
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.MYSQL_PASSWORD,
+  database: "todo_app",
+});
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json("Hello, this is the server :3");
+});
+
+app.get("/todos", (req, res) => {
+  const q = "select * from tasks";
+
+  db.query(q, (err, result) => {
+    if (err) return res.json(err);
+    return res.json(result);
+  });
+});
+
+app.post("/todos", (req, res) => {
+  const task = req.body.task;
+  const q = "insert into tasks (task) values (?)";
+
+  db.query(q, [task], (err, result) => {
+    if (err) return res.json(err);
+    return res.json("Task added!");
+  });
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const q = "delete from tasks where id = ?";
+
+  db.query(q, [id], (err, result) => {
+    if (err) return res.json(err);
+    return res.json("Task deleted!");
+  });
 });
 
 app.listen(8080, () => {
