76087e6c6633d7a8a4e77439cded59cef95da4bd
leanghok120 <leanghokoeng5@gmail.com>
2024-10-28 05:14:23 +0700

    Add padding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
+    <div className="h-screen flex flex-col justify-center items-center p-5">
       <h1 className="font-black text-center text-6xl p-3">Todo List</h1>
       <Todo />
     </div>
