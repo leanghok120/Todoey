468811edd356710dd856bbd22896f644e0b5d8d3
leanghok120 <leanghokoeng5@gmail.com>
2024-10-29 04:54:12 +0700

    Add User model

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
   task: {
     type: String,
     required: true,
