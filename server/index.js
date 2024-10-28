27f474b76674f895de0319cd44402a45d7d4453b
leanghok120 <leanghokoeng5@gmail.com>
2024-10-28 19:06:33 +0700

    Remove dist

--- server/index.js
+++ server/index.js
@@ -2,12 +2,6 @@ import express from "express";
 import cors from "cors";
 import mysql from "mysql";
 import "dotenv/config";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Define __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -20,10 +14,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "dist")));
-
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/index.html"));
+  res.json("This is the server :3");
 });
 
 app.get("/todos", (req, res) => {
