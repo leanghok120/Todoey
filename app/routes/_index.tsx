5c2b4a85cc20ac4ba605a3b08d4af260480fc78e
Leanghok <leanghokoeng5@gmail.com>
2024-12-18 04:49:22 +0700

    Update the UI a bit more

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -17,14 +17,27 @@ export default function Index() {
   return (
     <>
       <h1 className="font-bold text-5xl">Todoey</h1>
-      <main className="w-full max-w-96 flex flex-col justify-center items-center gap-4 mt-8 p-5 rounded-xl border-2 shadow-lg">
+      <main className="w-full max-w-96 flex flex-col justify-center items-center gap-4 mt-8 p-8 rounded-xl border shadow-md">
         <Form className="flex gap-2">
           <Input placeholder="What do you want to do?" />
           <Button>Add</Button>
         </Form>
-        <ul className="text-center">
-          <li>Learn to code</li>
-          <li>Build a todo list app</li>
+        <ul className="text-center space-y-3 w-3/4">
+          <li>
+            <Button className="w-full" variant="outline">
+              Learn remix
+            </Button>
+          </li>
+          <li>
+            <Button className="w-full" variant="outline">
+              Learn drizzle
+            </Button>
+          </li>
+          <li>
+            <Button className="w-full" variant="outline">
+              Build a todo app
+            </Button>
+          </li>
         </ul>
       </main>
     </>
